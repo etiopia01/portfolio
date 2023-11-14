1b717a24093f78cd02c8a2cfc550f6f47f126059
etiopia01 <etiopiasalvatore@gmail.com>
2023-11-14 15:11:41 +0100

    web style done

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -9,15 +9,15 @@ export default function HomePage() {
 
     return (
         <div className="h-[600px] overflow-scroll">
-        <div className="flex flex-col-reverse justify-center items-center gap-12 p-16">
-            <div className="flex flex-col justify-center items-center gap-10 ">
+        <div className="flex flex-col-reverse justify-center items-center gap-16 p-16 lg:flex-row">
+            <div className="flex flex-col justify-center items-center gap-12 lg:items-start lg:px-10">
                 <div className="flex gap-4 items-center">
                     <div className="w-32 h-32 rounded-full bg-[url(/me.jpeg)] bg-cover bg-center">
                        
                     </div>
                 <h2 className="tracking-wider text-main-text text-2xl text-opacity-80 font-semiboldbold">Salvatore Etiopia</h2>
                 </div>
-                <p className={`${merri.className} text-sm leading-7 font-extralight text-opacity-80 text-main-text`}> Hi! I&apos;m an aspiring Web Developer.
+                <p className={`${merri.className} text-sm leading-7 font-extralight text-opacity-90 text-main-text`}> Hi! I&apos;m an aspiring Web Developer.
                      In early 2023, I earned a Bachelor&apos;s degree in languages and literatures.
                       However, my passion for coding led me to swiftly change my career path.
                        Now, I&apos;m on an exciting journey to master the art of web development. 
@@ -26,9 +26,9 @@ export default function HomePage() {
                         My current skill-set consists mainly of the following front-end technologies: 
                         html5, CSS, tailwindcss, TypeScript, React and Next.js.
                         Let&apos;s make something happen!</p>
-                        <Link href={"/projects"}><button className="bg-icon text-main-text text-opacity-70 py-2 px-6 rounded-md hover:bg-opacity-70 hover:text-opacity-95">Take a look at my work</button> </Link>
+                        <Link href={"/projects"}><button className="bg-icon text-main-text text-opacity-70 py-3 px-8 text-lg rounded-md hover:bg-opacity-70 hover:text-opacity-95">Take a look at my work</button> </Link>
             </div>
-            <div className="text-main-text text-opacity-50 text-5xl flex flex-col justify-between items-center gap-4">
+            <div className="text-main-text text-opacity-50 text-5xl flex flex-col justify-between items-center gap-4 lg:text-7xl lg:items-start xl:text-8xl ">
                 <p>WAIT,</p>
                 <p> THIS IS </p>
                 <p>MY</p>
--- components/Project.tsx
+++ components/Project.tsx
@@ -2,15 +2,15 @@ import { ProjectProps } from "@/app/types"
 
 export default function Project(props:ProjectProps) {
     return (
-        <div className="relative"> 
+        <div className="relative mt-6"> 
         <div className="w-64 h-72 rounded-lg overflow-hidden flex flex-col gap-2 shadow-lg hover:shadow-2xl hover:scale-110 group peer hover:transition-all hover:duration-100 bg-main-text bg-opacity-40">
 <a href={props.vercel} className={`${props.class} bg-cover bg-center w-auto h-full`}></a>
 <div className="text-center p-3 hidden group-hover:block">
-<h1 className="font-bold text-main-bg">{props.title}</h1>
+<h1 className="font-bold text-[#0a0908] text-opacity-90 text-lg">{props.title}</h1>
 
 </div>
         </div>
-<div className="opacity-0 peer-hover:opacity-60 text-main-text peer-hover:transition-opacity w-64 h-auto text-xs absolute bottom-auto mt-8">{props.des}</div>
+<div className="opacity-0 peer-hover:opacity-60 text-main-text peer-hover:transition-opacity w-64 h-auto text-sm absolute bottom-auto mt-8">{props.des}</div>
 </div>
     )
 }
