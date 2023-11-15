cb03c6c27d267dc17727b28e3891ff01df70311f
etiopia01 <etiopiasalvatore@gmail.com>
2023-11-15 18:45:39 +0100

    added tip calc project

--- components/Project.tsx
+++ components/Project.tsx
@@ -3,15 +3,15 @@ import { merri } from "./HomePage"
 
 export default function Project(props:ProjectProps) {
     return (
-        <div className="relative mt-6"> 
-        <div className="w-64 h-72 rounded-lg overflow-hidden flex flex-col gap-2 shadow-lg hover:shadow-2xl hover:scale-110 group peer hover:transition-all hover:duration-100 bg-main-text bg-opacity-40">
+        <div className="relative lg:mt-12 mt-6"> 
+        <div className="lg:w-64 lg:h-72 w-48 h-60 rounded-lg overflow-hidden flex flex-col gap-2 shadow-lg hover:shadow-2xl hover:scale-110 group peer hover:transition-all hover:duration-100 bg-main-text bg-opacity-40">
 <a href={props.vercel} className={`${props.class} bg-cover bg-center w-auto h-full`}></a>
 <div className="text-center p-2 hidden group-hover:block">
 <h1 className="font-bold text-[#0a0908] text-opacity-90 text-lg">{props.title}</h1>
 
 </div>
         </div>
-<div className={`${merri.className} font-light opacity-0 peer-hover:opacity-60 text-main-text w-64 h-auto text-sm absolute bottom-auto mt-8`}>{props.des}</div>
+<div className={`${merri.className} font-light opacity-0 peer-hover:opacity-60 text-main-text lg:w-64 w-48 h-auto lg:text-sm text-xs absolute bottom-auto mt-8`}>{props.des}</div>
 </div>
     )
 }
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,27 +5,34 @@ const myProjects = [{
     github: "https://github.com/etiopia01/r-p-s-game",
     vercel: "https://r-p-s-game.vercel.app/",
     title: "Rock Paper Scissors Game",
-    des: "Simple r-p-s game I made when learning React",
+    des: "Simple r-p-s game I made when learning React, soon available on every game platform (probably)",
     id:1
 }, {
     class : "todo",
     github: "https://github.com/etiopia01/todo-list-tailwind",
     vercel: "https://todo-list-olive-nine-84.vercel.app/",
     title: "ToDo List",
-    des: "Simple ToDo list I made when first practicing Tailwindcss",
+    des: "Simple and very VERY original ToDo list I made when first practicing Tailwindcss",
     id:2
 },{
     class : "countries",
     github: "https://github.com/etiopia01/countries-list",
     vercel: "https://countries-list-theta.vercel.app/",
     title: "Countries Browser",
-    des: "One of the first projects I made using React with an API",
+    des: "Super useful project (seriously tho) I made using React with an API",
     id:3
+},{
+    class : "tip",
+    github: "https://github.com/etiopia01/tip-calculator",
+    vercel: "https://tip-calculator-seven-sepia.vercel.app/",
+    title: "Tip Calculator",
+    des: "Ok, shut up.. I was just practicing React",
+    id:4
 }]
 
 export default function Projects() {
     return (
-        <div className="flex flex-col items-center justify-start gap-10 mt-8 mx-20">
+        <div className="flex flex-col items-center justify-start gap-10 mt-8 mx-20 absolute left-0 right-0 top-20 bottom-20">
             <h1 className="text-3xl font-bold text-main-text text-opacity-60">MY PROJECTS</h1>
             <div className="flex justify-center flex-wrap w-auto gap-16 p-10 overflow-y-scroll h-[500px]">
             {myProjects.map(project => <Project key={project.id} {...project}/>)}
